671bacfab2034ff74833591f6a27ac107f2cbfd8
YergaliyevIssa <isa.ergaliev9@gmail.com>
2021-08-13 10:41:16 +0600

    Now we read api code from file

--- src/erlang_v8_lib_run.erl
+++ src/erlang_v8_lib_run.erl
@@ -3,138 +3,141 @@
 -export([run/2, pre_run/1, post_run/1, run/3]).
 
 system_code() ->
-    <<"
-        let handlers = {
-            api : 'fp_js_backend',
-            db  : 'fp_js_backend'
-        };
-        let fp_api = {
-            api:{
-                get_value:async ( Tag, Field, Default )=>{
-                    if (Default === undefined) {
-                        Args = [Tag, Field];
-                    } else {
-                        Args = [Tag, Field, Default]
-                    }
-                    Params = {path:['fp', 'api', 'get_value'], args: Args};
-                  let value = await external.run(handlers.api, Params);
-                  return value;
-                },
-                set_value:async (Tag, Field, Value) => {
-                    Params = {path:['fp', 'api', 'set_value'], args: [Tag, Field, Value]};
-                    let exec_code = await external.run(handlers.api, Params);
-                    return exec_code;
-              },
-                archives_get:async (Points, Archives) => {
-                    Params = {path:['fp', 'api', 'archives_get'], args: [Points, Archives]};
-                    let exec_code = await external.run(handlers.api, Params);
-                    return exec_code;
-                },
-                archives_get_periods:async (ParamS, Archives) =>  {
-                    Params = {path:['fp', 'api', 'archives_get_periods'], args: [ParamS, Archives]};
-                    let exec_code = await external.run(handlers.api, Params);
-                    return exec_code;
-                },
-                catalog_find_node: async (Catalog, NodeID) => {
-                    Params = {path:['fp', 'api', 'catalog_find_node'], args: [Catalog, NodeID]};
-                    let exec_code = await external.run(handlers.api, Params);
-                    return exec_code;
-                },
-                log: async (Level, Text, Arguments) => {
-                    if (Arguments === undefined) {
-                        Args = [Level, Text];
-                    } else {
-                        Args = [Level, Text, Arguments];
-                    }
-                    Params = {path : ['fp', 'api', 'log'], args: Args};
-                    let exec_code = await external.run(handlers.api, Params);
-                    return exec_code;
-                }
-            },
-            db:{
-                create_object:async (Fields) => {
-                    Params = {path:['fp', 'db', 'create_object'], args:[Fields]};
-                    let exec_code = await external.run(handlers.db, Params);
-                    return exec_code;
-                },
-                edit_or_create:async (Fields) => {
-                    Params = {path:['fp', 'db', 'edit_or_create'], args:[Fields]};
-                    let exec_code = await external.run(handlers.db, Params);
-                    return exec_code;
-                },
-
-                read_field:async (Object, Field) => {
-                    Args = [Object, Field];
-                    Params = {path:['fp', 'db', 'read_field'], args: Args};
-                    let exec_code = await external.run(handlers.db, Params);
-                    return exec_code;
-                },
-                read_fields:async (Object, Fields) => {
-                    Args = [Object, Fields];
-                    Params = {path:['fp', 'db', 'read_fields'], args: Args};
-                    let exec_code = await external.run(handlers.db, Params);
-                    return exec_code;
-                },
-                edit_object:async (Object, Fields) => {
-                    Args = [Object, Fields];
-                    Params = {path:['fp', 'db', 'edit_object'], args: Args};
-                    let exec_code = await external.run(handlers.db, Params);
-                    return exec_code;
-                },
-                dirty_edit_object:async (Object, Fields) => {
-                    Args = [Object, Fields];
-                    Params = {path:['fp', 'db', 'dirty_edit_object'], args: Args};
-                    let exec_code = await external.run(handlers.db, Params);
-                    return(exec_code);
-                },
-                copy_object:async (ID, Overwrite) => {
-                    Params = {path:['fp', 'db', 'copy_object'], args: [ID, Overwrite]};
-                    let exec_code = await external.run(handlers.db, Params);
-                    return(exec_code);
-                },
-                query: async(QueryString) => {
-                    Params = {path:['fp', 'db', 'query'], args: [QueryString]};
-                    let result = await external.run(handlers.db, Params);
-                    return result;
-                },
-                transaction:async (Fun) => {
-                    return 1;
-                },
-                start_transaction:async () => {
-                    Params = {path:['fp', 'db', 'start_transaction'], args:[]};
-                    let exec_code = await external.run(handlers.db, Params);
-                    return(exec_code);
-                },
-                commit_transaction:async () => {
-                    Params = {path:['fp', 'db', 'commit_transaction'], args:[]};
-                    let exec_code = await external.run(handlers.db, Params);
-                    return exec_code;
-                },
-                rollback_transaction:async () => {
-                    Params = {path:['fp', 'db', 'rollback_transaction'], args:[]};
-                    let exec_code = await external.run(handlers.db, Params);
-                    return exec_code;
-                },
-                to_oid:async (Object) => {
-                    Params = {path:['fp', 'db', 'to_oid'], args: [Object]};
-                    let value = await external.run(handlers.db, Params);
-                    return value;
-                },
-                path2oid:async (Path) => {
-                    Params = {path:['fp', 'db', 'path2oid'], args: [Path]};
-                    let value = await external.run(handlers.db, Params);
-                    return value;
-                },
-                to_path: async (Object) => {
-                    Params = {path:['fp', 'db', 'to_path'], args: [Object]};
-                    let value = await external.run(handlers.db, Params);
-                    return value;
-                }
-
-            }
-        };
-        let x = 100;
-    ">>.
+    SourcePath = code:priv_dir(fp) ++ "/UI/dev/backend_api/fp_backend_api.js",
+    {ok, Api} = file:read_file(SourcePath),
+    Api.
+%%    <<"
+%%        let handlers = {
+%%            api : 'fp_js_backend',
+%%            db  : 'fp_js_backend'
+%%        };
+%%        let fp_api = {
+%%            api:{
+%%                get_value:async ( Tag, Field, Default )=>{
+%%                    if (Default === undefined) {
+%%                        Args = [Tag, Field];
+%%                    } else {
+%%                        Args = [Tag, Field, Default]
+%%                    }
+%%                    Params = {path:['fp', 'api', 'get_value'], args: Args};
+%%                  let value = await external.run(handlers.api, Params);
+%%                  return value;
+%%                },
+%%                set_value:async (Tag, Field, Value) => {
+%%                    Params = {path:['fp', 'api', 'set_value'], args: [Tag, Field, Value]};
+%%                    let exec_code = await external.run(handlers.api, Params);
+%%                    return exec_code;
+%%              },
+%%                archives_get:async (Points, Archives) => {
+%%                    Params = {path:['fp', 'api', 'archives_get'], args: [Points, Archives]};
+%%                    let exec_code = await external.run(handlers.api, Params);
+%%                    return exec_code;
+%%                },
+%%                archives_get_periods:async (ParamS, Archives) =>  {
+%%                    Params = {path:['fp', 'api', 'archives_get_periods'], args: [ParamS, Archives]};
+%%                    let exec_code = await external.run(handlers.api, Params);
+%%                    return exec_code;
+%%                },
+%%                catalog_find_node: async (Catalog, NodeID) => {
+%%                    Params = {path:['fp', 'api', 'catalog_find_node'], args: [Catalog, NodeID]};
+%%                    let exec_code = await external.run(handlers.api, Params);
+%%                    return exec_code;
+%%                },
+%%                log: async (Level, Text, Arguments) => {
+%%                    if (Arguments === undefined) {
+%%                        Args = [Level, Text];
+%%                    } else {
+%%                        Args = [Level, Text, Arguments];
+%%                    }
+%%                    Params = {path : ['fp', 'api', 'log'], args: Args};
+%%                    let exec_code = await external.run(handlers.api, Params);
+%%                    return exec_code;
+%%                }
+%%            },
+%%            db:{
+%%                create_object:async (Fields) => {
+%%                    Params = {path:['fp', 'db', 'create_object'], args:[Fields]};
+%%                    let exec_code = await external.run(handlers.db, Params);
+%%                    return exec_code;
+%%                },
+%%                edit_or_create:async (Fields) => {
+%%                    Params = {path:['fp', 'db', 'edit_or_create'], args:[Fields]};
+%%                    let exec_code = await external.run(handlers.db, Params);
+%%                    return exec_code;
+%%                },
+%%
+%%                read_field:async (Object, Field) => {
+%%                    Args = [Object, Field];
+%%                    Params = {path:['fp', 'db', 'read_field'], args: Args};
+%%                    let exec_code = await external.run(handlers.db, Params);
+%%                    return exec_code;
+%%                },
+%%                read_fields:async (Object, Fields) => {
+%%                    Args = [Object, Fields];
+%%                    Params = {path:['fp', 'db', 'read_fields'], args: Args};
+%%                    let exec_code = await external.run(handlers.db, Params);
+%%                    return exec_code;
+%%                },
+%%                edit_object:async (Object, Fields) => {
+%%                    Args = [Object, Fields];
+%%                    Params = {path:['fp', 'db', 'edit_object'], args: Args};
+%%                    let exec_code = await external.run(handlers.db, Params);
+%%                    return exec_code;
+%%                },
+%%                dirty_edit_object:async (Object, Fields) => {
+%%                    Args = [Object, Fields];
+%%                    Params = {path:['fp', 'db', 'dirty_edit_object'], args: Args};
+%%                    let exec_code = await external.run(handlers.db, Params);
+%%                    return(exec_code);
+%%                },
+%%                copy_object:async (ID, Overwrite) => {
+%%                    Params = {path:['fp', 'db', 'copy_object'], args: [ID, Overwrite]};
+%%                    let exec_code = await external.run(handlers.db, Params);
+%%                    return(exec_code);
+%%                },
+%%                query: async(QueryString) => {
+%%                    Params = {path:['fp', 'db', 'query'], args: [QueryString]};
+%%                    let result = await external.run(handlers.db, Params);
+%%                    return result;
+%%                },
+%%                transaction:async (Fun) => {
+%%                    return 1;
+%%                },
+%%                start_transaction:async () => {
+%%                    Params = {path:['fp', 'db', 'start_transaction'], args:[]};
+%%                    let exec_code = await external.run(handlers.db, Params);
+%%                    return(exec_code);
+%%                },
+%%                commit_transaction:async () => {
+%%                    Params = {path:['fp', 'db', 'commit_transaction'], args:[]};
+%%                    let exec_code = await external.run(handlers.db, Params);
+%%                    return exec_code;
+%%                },
+%%                rollback_transaction:async () => {
+%%                    Params = {path:['fp', 'db', 'rollback_transaction'], args:[]};
+%%                    let exec_code = await external.run(handlers.db, Params);
+%%                    return exec_code;
+%%                },
+%%                to_oid:async (Object) => {
+%%                    Params = {path:['fp', 'db', 'to_oid'], args: [Object]};
+%%                    let value = await external.run(handlers.db, Params);
+%%                    return value;
+%%                },
+%%                path2oid:async (Path) => {
+%%                    Params = {path:['fp', 'db', 'path2oid'], args: [Path]};
+%%                    let value = await external.run(handlers.db, Params);
+%%                    return value;
+%%                },
+%%                to_path: async (Object) => {
+%%                    Params = {path:['fp', 'db', 'to_path'], args: [Object]};
+%%                    let value = await external.run(handlers.db, Params);
+%%                    return value;
+%%                }
+%%
+%%            }
+%%        };
+%%        let x = 100;
+%%    ">>.
 
 pre_run(Opts) ->
     HandlerContext = maps:get(handler_context, Opts, #{}),
